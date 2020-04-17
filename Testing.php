568caeae3dc0508dc5c12f2b0fdefffc42ce3248
John Saunders <john@yettimedia.co.uk>
2020-04-17 17:53:06 +0100

    fixed test

--- Testing.php
+++ Testing.php
@@ -6,12 +6,16 @@ include './vendor/autoload.php';
 
 ShardMatrix::initFromYaml( __DIR__ . '/shard_matrix.yaml' );
 
-$node = ShardMatrix::getConfig()->getNodes()->getNodes()[4];
-//make NODE distributer
-$table = new \ShardMatrix\Table( 'visitors');
+//$node = ShardMatrix::getConfig()->getNodes()->getNodes()[4];
+////make NODE distributer
+//$table = new \ShardMatrix\Table( 'visitors');
+//
+//$uuid = \ShardMatrix\Uuid::make( $node, $table);
+//
+//echo $uuid->getNode()->getName();
+//echo $uuid->getTable()->getName();
+//echo $uuid;
 
-$uuid = \ShardMatrix\Uuid::make( $node, $table);
+$uuid = new \ShardMatrix\Uuid('0fca0384-1ea80cba-e8a2-6a8c-bacd-444230303035');
 
-echo $uuid->getNode()->getName();
-echo $uuid->getTable()->getName();
-echo $uuid;
+echo $uuid->getTable()->getName().' '.$uuid->getNode()->getName();
