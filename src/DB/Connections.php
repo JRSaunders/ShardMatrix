b80f4b51df110572cee125c6d14775f3b2730afd
John Saunders <john@yettimedia.co.uk>
2020-04-18 15:43:06 +0100

    db attributes

--- src/DB/Connections.php
+++ src/DB/Connections.php
@@ -11,12 +11,22 @@ use ShardMatrix\ShardMatrix;
 class Connections {
 
 	protected static $connections = [];
+	protected static $dbAttributes = [];
+
+	public static function setDbAttributes( array $dbAttributes ): void {
+		static::$dbAttributes = $dbAttributes;
+	}
 
 	public static function getNodeConnection( Node $node ): \PDO {
 		if ( isset( static::$connections[ $node->getName() ] ) ) {
 			return static::$connections[ $node->getName() ];
 		}
 		$db = new \PDO( $node->getDsn(), $node->getUsername(), $node->getPassword() );
+
+		foreach ( static::$dbAttributes as $attribute => $value ) {
+			$db->setAttribute( $attribute, $value );
+		}
+
 		return static::$connections[ $node->getName() ] = $db;
 
 	}
